41d6ed31aacd02e7a50ad5fd9596c4ac24520f02
mayfra.useche@gmail.com <42663497+mayfrauseche@users.noreply.github.com>
2020-07-09 12:20:15 -0500

    Se uso el ciclo for, acum y number

--- promedio.js
+++ promedio.js
@@ -1,1 +1,9 @@
-var materia = prompt ('Ingresa el nombre de tu materia')
+//El programa pedira 5 notas y te va a devoler el promedio de las 5 notas
+var acum = 0;
+for(var index = 0; index < 5; index++){
+    var edad = Number(prompt(`Ingresa la nota: ${index}`));
+    acum += edad;
+}
+
+var promedio = acum / 5;
+console.log(`Promedio de notas es: ${promedio}`);
