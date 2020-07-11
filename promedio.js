9a0f04b2b26ccdf8f4b570397bbf734baec4882c
mayfra.useche@gmail.com <42663497+mayfrauseche@users.noreply.github.com>
2020-07-11 09:37:01 -0500

    Modificando linea de codigo

--- promedio.js
+++ promedio.js
@@ -1,9 +1,21 @@
 //El programa pedira 5 notas y te va a devoler el promedio de las 5 notas
 var acum = 0;
-for(var index = 0; index < 5; index++){
-    var edad = Number(prompt(`Ingresa la nota: ${index}`));
-    acum += edad;
+for(var index = 0; index <= 5; index++){
+    var nota = Number(prompt(`Ingresa la nota: ${index}`));
+    acum += nota;
 }
 
-var promedio = acum / 5;
+while (nota * prompt){
+    alert (promtp = confirm)
+}
+
+var continuar = confirm('Desea continuar con más promedios?')
+    if (continuar) {
+        var nota = Number(prompt(`Ingresa la nota: ${index}`));
+    } else {
+        (cancelar)
+    }
+
+
+var promedio = acum / 5
 console.log(`Promedio de notas es: ${promedio}`);
